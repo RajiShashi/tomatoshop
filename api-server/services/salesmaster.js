458042c918b335af459a0de4a4b17d24a2700f3b
sivapgv <sivapgv@gmail.com>
2023-03-14 08:40:34 +0530

    Edit api update

--- api-server/services/salesmaster.js
+++ api-server/services/salesmaster.js
@@ -3,10 +3,136 @@ const helper = require('../helper');
 const config = require('../config');
 
 
-async function createsalesmaster(request){
+async function createsalesmaster(request) {
+  if (request.sales['id']) {
+    // update data
+    const salesid = request.sales['id'];
+    const result = await db.query(
+      `Update masters set
+       date = '${request.sales.date}', 
+        credit = '${request.sales.credit}', 
+        debit = '${request.sales.debit}', 
+        billdate ='${request.sales.date}', 
+        amount =  '${request.sales.totalamount}', 
+        commission = '${request.sales.commission}',
+        rent ='${request.sales.rent}',
+        cooly = '${request.sales.wages}',
+        sungam = '${request.sales.toll}'
+      `
+    );
 
-  const result = await db.query(
-    `INSERT INTO masters 
+    let message = "salesmaster error";
+
+    if (result.affectedRows) {
+      message = 'Salesmaster created successfully';
+    }
+    // Inwards data update
+    for (var i = 0; i < request.inwards.length; i++) {
+      if (request.inwards[i].id) {
+        await db.query(`DELETE FROM purchase where id=${request.inwards[i].id}`);
+        await db.query(`DELETE FROM purchase where refno=${request.inwards[i].id}`);
+      }
+      const result_inwords = await db.query(
+        `INSERT INTO purchase 
+       (date,
+        refno, 
+        cname, 
+        pname, 
+        kgs, 
+        pack,
+        rate,
+        amount,
+        pcode,
+        category,
+        commission,
+        rent,
+        cooly,
+        sungam,
+        code) 
+      VALUES 
+      ( '${request.sales.date}',
+        '${salesid}',
+        '${request.sales.customerid}',
+        '${request.inwards[i].product}', 
+        '${request.inwards[i].kgs}',
+        '${request.inwards[i].pack}',
+        '${request.inwards[i].rate}',
+        '${request.inwards[i].amount}',
+        '${request.inwards[i].product}',
+        'KGS',
+        '${request.sales.commission}',
+        '${request.sales.rent}',
+        '${request.sales.wages}',
+        '${request.sales.toll}',
+        '1'
+        )`
+      );
+
+      const purchaseid = result_inwords.insertId;
+      for (var j = 0; j < request.inwards[i].packValue.length; j++) {
+        const result_pack = await db.query(
+          `INSERT INTO packpurchase 
+           (date,
+            refno, 
+            product, 
+            qty,
+            sno
+           )
+           VALUES 
+           ( '${request.sales.date}',
+             '${purchaseid}',
+             '${request.inwards[i].product}',
+             '${request.inwards[i].packValue[j].modalPack}',
+             '${j + 1}'
+           )`
+        );
+
+      }
+
+    }
+
+    for (var i = 0; i < request.outwards.length; i++) {
+      if (request.outwards[i].id) {
+        await db.query(`DELETE FROM sales where id=${request.outwards[i].id}`);
+      }
+      const result_outwards = await db.query(
+        `INSERT INTO sales 
+         (date,
+          refno,
+          cname, 
+          businessmen, 
+          kgs, 
+          pack, 
+          rate,
+          amount,
+          pname,
+          category,
+          code
+          ) 
+        VALUES 
+        ( '${request.sales.date}',
+          '${salesid}',
+          '${request.sales.customerid}',
+          '${request.outwards[i].businessMan}',
+          '${request.outwards[i].outKgs}',
+          '${request.outwards[i].outPack}', 
+          '${request.outwards[i].outRate}',
+          '${request.outwards[i].outAmount}',
+          '${request.outwards[i].outProduct}',
+          'KGS',
+          '1'
+          )`
+      );
+    }
+
+    return { message };
+
+
+
+  } else {
+    // insert data
+    const result = await db.query(
+      `INSERT INTO masters 
      (date, 
       name, 
       credit, 
@@ -40,23 +166,23 @@ async function createsalesmaster(request){
     '${request.sales.wages}',
     '${request.sales.toll}'
      )`
-  );
+    );
 
-  let message = "salesmaster error";
+    let message = "salesmaster error";
 
-  if (result.affectedRows) {
-    message = 'Salesmaster created successfully';
-  }
+    if (result.affectedRows) {
+      message = 'Salesmaster created successfully';
+    }
 
-  const salesid = result.insertId;
-  const getrefno = await db.query(
-    `SELECT refno from purchase ORDER BY refno DESC LIMIT 1`
-  );
-  
-  //const refno = getrefno[0].refno+1;
-  for(var i=0;i<request.inwards.length;i++) {
-  const result_inwords = await db.query(
-    `INSERT INTO purchase 
+    const salesid = result.insertId;
+    const getrefno = await db.query(
+      `SELECT refno from purchase ORDER BY refno DESC LIMIT 1`
+    );
+
+    //const refno = getrefno[0].refno+1;
+    for (var i = 0; i < request.inwards.length; i++) {
+      const result_inwords = await db.query(
+        `INSERT INTO purchase 
      (date,
       refno, 
       cname, 
@@ -89,12 +215,12 @@ async function createsalesmaster(request){
       '${request.sales.toll}',
       '1'
       )`
-    );
+      );
 
-    const purchaseid = result_inwords.insertId;
-    for(var j=0;j<request.inwards[i].packValue.length;j++) {
-      const result_pack = await db.query(
-        `INSERT INTO packpurchase 
+      const purchaseid = result_inwords.insertId;
+      for (var j = 0; j < request.inwards[i].packValue.length; j++) {
+        const result_pack = await db.query(
+          `INSERT INTO packpurchase 
          (date,
           refno, 
           product, 
@@ -106,18 +232,18 @@ async function createsalesmaster(request){
            '${purchaseid}',
            '${request.inwards[i].product}',
            '${request.inwards[i].packValue[j].modalPack}',
-           '${j+1}'
+           '${j + 1}'
          )`
-      ); 
-             
+        );
+
+      }
+
     }
 
-  }
 
-
-  for(var i=0;i<request.outwards.length;i++) {
-    const result_outwards = await db.query(
-      `INSERT INTO sales 
+    for (var i = 0; i < request.outwards.length; i++) {
+      const result_outwards = await db.query(
+        `INSERT INTO sales 
        (date,
         refno,
         cname, 
@@ -146,32 +272,33 @@ async function createsalesmaster(request){
       );
     }
 
-  return {message};
+    return { message };
+  }
 }
 
 async function getSalesId(id) {
   let condition = '';
   let data = '';
-  if(id==0){
+  if (id == 0) {
     condition = 'ORDER BY billno DESC LIMIT 1';
     const rows = await db.query(
       `SELECT * FROM masters ${condition}`
     );
     data = helper.emptyOrRows(rows);
   } else {
-    condition = 'where id='+id;
+    condition = 'where id=' + id;
     const editrows = await db.query(
       `SELECT * FROM masters ${condition}`
     );
     const masterdata = helper.emptyOrRows(editrows);
 
-    condition = 'where refno='+id;
+    condition = 'where refno=' + id;
     const purcharows = await db.query(
       `SELECT * FROM purchase ${condition}`
     );
     let purchasedata = helper.emptyOrRows(purcharows);
     let packdata = '';
-    for(var i=0;i<purchasedata.length;i++) {
+    for (var i = 0; i < purchasedata.length; i++) {
       const packrows = await db.query(
         `SELECT * FROM 	packpurchase ${condition}`
       );
@@ -187,8 +314,8 @@ async function getSalesId(id) {
 
     data = {
       sales: salesdata[0],
-      inword: purchasedata,
-      outowrd: salesdata
+      inwords: purchasedata,
+      outowrds: salesdata
     }
   }
   return data;
